b3d94a2c2ae71e454261a0634a9d0014b901b51c
Stratomar Designs <David@stratomar.com>
2025-10-22 00:14:05 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
     return [{
@@ -10,4 +9,4 @@ const nextConfig = {
     }]
   }
 }
-module.exports = nextConfig
+module.exports = nextConfig;
